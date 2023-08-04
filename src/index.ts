276bf155b5ae21874174cd8ace23cddf401c4ca1
Juan Curti <jmfcdev@gmail.com>
2023-08-04 14:10:32 +0200

    Tests added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-async function validateVAT(code: string) {
-    if (code.length == 0) {return false};
+import { createClient } from 'soap';
+const euUrl = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService.wsdl";
+
+async function validateVAT(code: string, countryCode: string):Promise<boolean | void> {
+    if (code.length == 0 || countryCode.length == 0) {return false};
+    
+    return new Promise(async(resolve, reject) => {
+        await createClient(euUrl, function(err, client) {
+            if(err) {
+                reject(err);
+            }else{
+                const _args = { vatNumber: code, countryCode: countryCode };
+            
+                client.checkVat(_args, async function(errArg: any, result: any) {
+                    if(errArg) {
+                        reject(errArg);
+                    }
+                    
+                    resolve(result.valid);
+                })
+            }
+        });
+    })
 }
 
 export {
