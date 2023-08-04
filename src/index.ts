ac3c8a8266bbb2a699af99167aca2441a9014c74
Juan Curti <jmfcdev@gmail.com>
2023-08-04 14:37:54 +0200

    Change formatting of arguments

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import { createClient } from 'soap';
 const euUrl = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService.wsdl";
 
-async function validateVAT(code: string, countryCode: string):Promise<boolean | void> {
-    if (code.length == 0 || countryCode.length == 0) { return false };
+async function validateVAT(fullCode: string):Promise<boolean | void> {
+    if (fullCode.length < 2) { return false };
+    var cleanCode = fullCode.split(' ').join('');
+    let countryCode = cleanCode.substring(0, 2);
+    let code = cleanCode.substring(2, cleanCode.length);
     
     return new Promise(async(resolve, reject) => {
         await createClient(euUrl, function(err, client) {
             if(err) {
+                console.log(err);
                 reject(err);
             }else{
                 const _args = { vatNumber: code, countryCode: countryCode };
             
                 client.checkVat(_args, async function(errArg: any, result: any) {
                     if(errArg) {
+                        console.log(errArg)
                         reject(errArg);
                     }
                     
